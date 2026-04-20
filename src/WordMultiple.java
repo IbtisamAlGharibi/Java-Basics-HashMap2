d342f16940682c357e387da4a3d4f724528972e1
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 11:37:17 +0400

    if statement is written inside the loop

--- src/WordMultiple.java
+++ src/WordMultiple.java
@@ -5,13 +5,18 @@ public class WordMultiple {
     public static void main(String[] args){
 
         String[] array = {"a", "b", "a", "c", "b"};
-        Map<String, Integer> wordMultiple = new HashMap<>();
+        Map<String, Boolean> wordMultiple = new HashMap<>();
 
-        String result = "";
 
         for (int i = 0; i < array.length; i++) {
 
+            String key = array[i];
 
+            if (wordMultiple.containsKey(key)) {
+                wordMultiple.put(key, true);
+            } else {
+                wordMultiple.put(key, false);
+            }
         }
 
     }
