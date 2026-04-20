67acc95f0081c6cb0b51473408f135efd4d3029b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 11:52:53 +0400

    if statement is written inside the loop

--- src/AllSwap.java
+++ src/AllSwap.java
@@ -8,7 +8,21 @@ public class AllSwap {
 
         for (int i = 0; i < array.length; i++) {
 
+            String key = array[i].substring(0, 1);
 
+            if (allSwap.containsKey(key)) {
+
+                int index = allSwap.get(key);
+
+                String temp = array[i];
+                array[i] = array[index];
+                array[index] = temp;
+
+                allSwap.remove(key);
+
+            } else {
+                allSwap.put(key, i);
+            }
         }
 
 
