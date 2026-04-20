91684c20e9fac43a17bba50a407c0cf144746be8
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 11:08:06 +0400

    if statement is written inside the loop

--- src/FirstChar.java
+++ src/FirstChar.java
@@ -5,13 +5,24 @@ public class FirstChar {
     public static void main(String[] args){
 
         String[] array = {"salt", "tea", "soda", "toast"};
-        Map<String,Integer > firstChar = new HashMap<>();
+        Map<String,String > firstChar = new HashMap<>();
 
         for (int i =0; i< array.length;i++){
 
-            String key = array[0];
+            String arr = array[i];
+            String key = arr.substring(0,1);
 
+            if (firstChar.containsKey(key)){
+                firstChar.put(key, firstChar.get(key) + arr);
+            }else {
+
+                firstChar.put(key, arr);
+            }
 
         }
+        System.out.println(firstChar);
+
     }
+
 }
+
