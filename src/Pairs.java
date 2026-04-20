12ff9ba414e0705fcd9dbadfbffe6f403ab6ee37
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 09:40:08 +0400

    2 substrings are declared inside the loop

--- src/Pairs.java
+++ src/Pairs.java
@@ -4,6 +4,14 @@ import java.util.Map;
 public class Pairs {
     public static void main(String[] args){
         String[] array = {"code", "bug"};
+
         Map<String,Integer> pairs = new HashMap<>();
+
+        for (String arr : array){
+            String first = arr.substring(0,1);
+            String last = arr.substring(arr.length()-1);
+
+
+        }
     }
 }
