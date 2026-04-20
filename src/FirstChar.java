cf9b511f15352a6e9be9857c90e419fd2cf5736c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 10:54:14 +0400

    new HashMap is created

--- src/FirstChar.java
+++ src/FirstChar.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstChar {
     public static void main(String[] args){
 
         String[] array = {"salt", "tea", "soda", "toast"};
+        Map<String,Integer > firstChar = new HashMap<>();
+
     }
 }
