7ce4c0ca1bcd29d8ce61cde9a9e517e9da20441f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 09:04:41 +0400

    for loop is created

--- src/Word0.java
+++ src/Word0.java
@@ -5,6 +5,9 @@ public class Word0 {
     public static void main(String[] args){
         String[] array={"a","b","c","d"};
         Map<String,Integer> word0 = new HashMap<>();
-
+        for (String arr: array){
+            word0.put(arr,0);
+        }
+        System.out.println(word0);
     }
 }
