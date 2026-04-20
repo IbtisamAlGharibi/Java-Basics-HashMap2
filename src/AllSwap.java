e09dd65a5fe5bf0238a2b74d692184fa4fe753e1
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 11:51:17 +0400

    string array is declared and New HashMap is created

---
+++ src/AllSwap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AllSwap {
+    public static void main(String[] args){
+        String[] array = {"ab", "ac"};
+        Map<String, Integer> allSwap = new HashMap<>();
+
+
+    }
+}
