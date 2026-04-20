1a29fd2f532c5a2c48503d7098196b7ba92f37f4
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 10:27:54 +0400

    new HashMap is created

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount {
     public static void main(String[] args){
         String[] array = {"a", "b", "a", "c", "b"};
+        Map<String,Integer > wordCount = new HashMap<>();
+
 
     }
 
