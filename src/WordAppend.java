1637d658f1940af241fc1541989366387d409c6a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 11:12:20 +0400

    new HashMap is created

--- src/WordAppend.java
+++ src/WordAppend.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordAppend {
     public static void main(String[] args){
         String[] array = {"a", "b", "a"};
-
+        Map<String,String > wordAppend = new HashMap<>();
 
     }
 }
