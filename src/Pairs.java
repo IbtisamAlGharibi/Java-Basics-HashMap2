c4ddcf3403d3d37a6735ac16c83f0db39439b185
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 09:32:28 +0400

    new HashMap is created

--- src/Pairs.java
+++ src/Pairs.java
@@ -1,7 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Pairs {
     public static void main(String[] args){
         String[] array = {"code", "bug"};
-
-
+        Map<String,Integer> pairs = new HashMap<>();
     }
 }
