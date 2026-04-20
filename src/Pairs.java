6573c080178a4b0fedd94093c66d508e0baaf42f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-20 10:22:35 +0400

    printing the HashMap on the screen

--- src/Pairs.java
+++ src/Pairs.java
@@ -5,13 +5,15 @@ public class Pairs {
     public static void main(String[] args){
         String[] array = {"code", "bug"};
 
-        Map<String,Integer> pairs = new HashMap<>();
+        Map<String,String > pairs = new HashMap<>();
 
         for (String arr : array){
             String first = arr.substring(0,1);
             String last = arr.substring(arr.length()-1);
 
+            pairs.put(first,last);
 
         }
+        System.out.println(pairs);
     }
 }
